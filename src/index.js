4fb49952ddefac5d4279d6abefb40e94b5a98727
micolka <mic_la@tut.by>
2020-10-01 22:20:19 +0300

    feat: resolve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let isDeleted = false;
+  let i = 0;
+
+  while (i < arr.length) {
+    isDeleted = false;
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+        arr.splice(i, 2);
+        isDeleted = true;
+      } 
+    }
+    isDeleted ? i = 0 : i++	
+  }
+  
+  return (arr.length === 0) ? true : false;
 }
